b522e00afcbd506d6674640f40c85ee185f27e0b
NAPTlME <napratt2@yahoo.com>
2021-02-24 11:46:29 -0600

    removed working directory changes, I didn't see the call to test_dir() in the instructions previously.

--- r/tests/test_check.R
+++ r/tests/test_check.R
@@ -1,8 +1,3 @@
-# Since, when opening this from the rproj, the parent directory is the working directory, 
-# will explicitely call setwd for clarity (assuming from default Github directory on a windows system)
-# Could simply change the source file call to reflect the project directory, 
-# but that would cause issues if this were to be run from the command line
-setwd("~/GitHub/code-challenges-env-check/r/tests")
 # Turned on printing when dealing with simple returns so the output can be verified
 source("../check.R", chdir=TRUE, print.eval = T) 
 library(testthat)
